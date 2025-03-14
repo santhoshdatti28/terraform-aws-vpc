e8df677f9712e05bba0b7e8d580bb61ee22667a2
sample <sample@gmail.com>
2025-03-14 20:55:09 +0530

    modules

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames= var.enable_dns_hostnames
+
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags,
+    {
+        Name= local.resource_name
+    }
+
+  )
+    
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "project_name"{
+
+}
+
+variable "environment_name"{
+
+}
+
+variable "vpc_cidr"{
+
+}
+
+variable "enable_dns_hostnames"{
+    default= true
+}
+
+variable "common_tags"{
+
+}
+
+variable "vpc_tags"{
+    default={}
+}
+
