5af35e8e47ede49b20d05eec263e5e780eb1cbae
sample <sample@gmail.com>
2025-03-18 18:32:04 +0530

    module

--- main.tf
+++ main.tf
@@ -131,19 +131,19 @@ resource "aws_route_table" "database" {
 
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0/16"
+  destination_cidr_block    = "0.0.0.0/0"
   gateway_id= aws_internet_gateway.main.id
 }
 
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0/16"
+  destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id= aws_nat_gateway.example.id
 }
 
 resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
-  destination_cidr_block    = "0.0.0.0/16"
+  destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id= aws_nat_gateway.example.id
 }
 
